1147504f038cc5611605b534248fb83599c21e37
shaunsargent <ShaunM10@gmail.com>
2023-12-19 22:11:58 -0500

    Added button with Toast mesage inside.

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -1,16 +1,20 @@
 package com.example.unitconverter
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.unitconverter.ui.theme.UnitConverterTheme
 
@@ -36,27 +40,40 @@ class MainActivity : ComponentActivity() {
 fun UnitConverter(){
     Column {
             // in a Column, all the UI elements will be stacked below each other.
+        Text("Unit Converter ")
+        OutlinedTextField(value = "", onValueChange ={
+            val myText = it
+            // Event handler code when value of the text is changed.
+        } )
         Row {
+            val context = LocalContext.current
             // in a Row, all the UI elements will be stacked next to each other.
-            Greeting(name = "Shaun")
-            Greeting(name = "Jack")
+           Button(onClick = {
+
+                Toast.makeText(context,
+                    "I was clicked",
+                    Toast.LENGTH_SHORT).show()
+
+           }) {
+               Text(text = "MyButton")
+           }
         }
 
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    UnitConverterTheme {
-        Greeting("Android")
-    }
+fun UnitConverterPreview(){
+    UnitConverter()
+
 }
+
+//@Preview(showBackground = true)
+//@Composable
+//fun GreetingPreview() {
+//    UnitConverterTheme {
+//        UnitConverter()
+//    }
+//}
