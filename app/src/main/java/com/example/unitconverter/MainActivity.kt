2be2fd4930656f27151f3fdd20ed14f0a2ebf91b
shaunsargent <ShaunM10@gmail.com>
2023-12-27 16:07:53 -0500

    Added spacers

--- app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -1,19 +1,22 @@
 package com.example.unitconverter
 
-import android.graphics.drawable.Icon
+
 import android.os.Bundle
-import android.widget.Toast
+
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowDropDown
 import androidx.compose.material3.Button
-import androidx.compose.material3.DropdownMenu
+import androidx.compose.ui.unit.dp
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
@@ -22,9 +25,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.VerticalAlignmentLine
-import androidx.compose.ui.modifier.modifierLocalConsumer
-import androidx.compose.ui.platform.LocalContext
+
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.unitconverter.ui.theme.UnitConverterTheme
 
@@ -53,12 +54,17 @@ fun UnitConverter(){
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-            // in a Column, all the UI elements will be stacked below each other.
+
+        // in a Column, all the UI elements will be stacked below each other.
+        Spacer(modifier = Modifier.height(16.dp))
         Text("Unit Converter ")
+
+        Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(value = "", onValueChange ={
             val myText = it
             // Event handler code when value of the text is changed.
         } )
+        Spacer(modifier = Modifier.height(16.dp))
         Row {
             // we need a box because the drop down needs a parent
             Box{
@@ -77,6 +83,8 @@ fun UnitConverter(){
                 }
             }
         }
+        Spacer(modifier = Modifier.height(16.dp))
+        Text("Result:")
     }
 }
 
